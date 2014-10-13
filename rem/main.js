37b32d2b0006f5998b5fa7ed2f078175282aff84
lisongyu <269876940@qq.com>
2014-10-13 09:50:47 +0800

    上传移动端设置字体的JS文件

---
+++ rem/main.js
@@ -0,0 +1,19 @@
+require(['zepto'], function () {
+//   $('html').css('fontSize','1rem');
+
+    var oHtml=$('html');
+    var phptoWid='';
+    var currentRem='';
+    function setPx(){
+        phptoWid=oHtml.width();
+        currentRem=phptoWid/320*20+'px';
+        oHtml.css('fontSize',currentRem);
+    }
+    setPx();
+
+
+    $(window).on('resize orientationchange').resize(function(){
+        setPx();
+    });
+//    alert(currentRem);
+});

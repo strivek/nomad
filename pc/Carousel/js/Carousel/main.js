6bb7d16669f087177e92a33bc578db1598b28454
chengyuan <chengyuan9826@qq.com>
2014-10-24 18:44:55 +0800

    轮播组件

---
+++ pc/Carousel/js/Carousel/main.js
@@ -0,0 +1,22 @@
+require(['jquery', 'Carousel/Carousel'], function () {
+        $('#carousel').flexslider({
+            animation: "slide",
+            controlNav: false,
+            animationLoop: false,
+            slideshow: false,
+            itemWidth: 210,
+            itemMargin: 5,
+            asNavFor: '#slider'
+        });
+
+        $('#slider').flexslider({
+            animation: "slide",
+            controlNav: false,
+            animationLoop: false,
+            slideshow: false,
+            sync: "#carousel",
+            start: function(slider){
+                $('body').removeClass('loading');
+            }
+        });
+})
---
+++ pc/Carousel/js/Carousel2/main.js
@@ -0,0 +1,6 @@
+require(['jquery', 'Carousel2/Carousel'], function () {
+    $('#basic').flexslider({
+        animation: "slide",
+
+    });
+})

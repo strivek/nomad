0827cfc6d37605f241962130a44ee85530b8764e
jingxijun <jingxijun@yeah.net>
2014-10-24 14:54:16 +0800

    update rem

--- web/mobile/rem/js/main.js
+++ web/mobile/rem/js/main.js
@@ -1,3 +1,4 @@
+/*
 require(['zepto'], function () {
 //   $('html').css('fontSize','1rem');
 
@@ -16,4 +17,20 @@ require(['zepto'], function () {
         setPx();
     });
 //    alert(currentRem);
+});*/
+$(function(){
+    var oHtml=$('html');
+    var phptoWid='';
+    var currentRem='';
+    function setPx(){
+        phptoWid=oHtml.width();
+        currentRem=phptoWid/320*20+'px';
+        oHtml.css('fontSize',currentRem);
+    }
+    setPx();
+
+
+    $(window).on('resize orientationchange').resize(function(){
+        setPx();
+    });
 });

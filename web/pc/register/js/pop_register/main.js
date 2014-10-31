413cb32fe320deed440bfcea1bc7134a0a6ac656
chengyuan <chengyuan9826@qq.com>
2014-10-31 20:49:49 +0800

    更新登录页面

--- web/pc/register/js/pop_register/main.js
+++ web/pc/register/js/pop_register/main.js
@@ -2,6 +2,9 @@ require(['jquery'], function () {
     var $pop_reg= $(".pop_reg");//弹出div
     var $close=$pop_reg.find(".close");//close 按钮
     var $form_input=$pop_reg.find(".form_input");//输入框
+    var $form_btn=$pop_reg.find(".button");//提交按钮
+    var $checked_div=$pop_reg.find(".xinxi");//包含复选框的div
+    var $checked=$checked_div.find(".radio a");//复选框
 
     //弹出注册框
    $(".regRadio").on("click",function(e){
@@ -25,7 +28,24 @@ require(['jquery'], function () {
     });
 
     //输入框按下
-    /*$('input')*/$form_input.on("keydown",function(event){
+    $form_input.on("keydown",function(event){
        $(this).siblings("i").hide();
     });
+
+    //单击复选框
+    $checked.on("click",function(){
+       $(this).toggleClass("checked");
+
+        //如果未点击复选框，提交按钮不可点击
+        if($checked.hasClass("checked")){
+              $form_btn.removeAttr("disabled").addClass("button_bg");
+
+        }
+        else{$form_btn.attr("disabled","disabled").removeClass("button_bg");}
+    });
+
+    $form_btn.on("click",function(){
+         alert("注册成功");
+    });
+
 })
